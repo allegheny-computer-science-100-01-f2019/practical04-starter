3b6470e38fa9782550ee61f6f464028a6e5091cb
jjumadin <jjumadin@aldenv64-6.local>
2019-09-25 23:42:44 -0400

    Starter materials for practical 4 in CMPSC 100-01 Fall 2019

---
+++ src/main/java/practicalfour/MadLibs.java
@@ -0,0 +1,38 @@
+package practicalfour;
+
+import java.util.Date;
+import java.util.Scanner;
+
+/** This class has a method that creates a Mid Libs story.
+ * The program utilizes the words in a text file.
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Add Your Name Here
+ */
+
+public class MadLibs {
+
+  /** The main entry point for MadLibs.
+   *  The source code inside of this method will be executed when the program is
+   *  run in the command-line. The output of this program will appear in the
+   *  terminal window.
+   *
+   **/
+  public static void main(String[] args) {
+    // display the name of the programmer and the date
+    System.out.println("Add Your Name Here " + new Date());
+    // declare and initialize the scanner
+    Scanner scanner = new Scanner(System.in);
+
+    // TODO: Modify the statement printed below to fit your needs
+    System.out.println("Please enter ... ");
+
+    // TODO: Read in and save the input from the terminal
+
+    // TODO: Display the words and numbers in a Mad Libs story
+
+    // TODO: Display a final thank you message
+  }
+
+}
